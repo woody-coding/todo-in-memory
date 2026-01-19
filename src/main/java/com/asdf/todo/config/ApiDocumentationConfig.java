5b7ad38443aa2ef298b17522761f200f97bc94d6
woody-coding <kingright0573@gmail.com>
2026-01-19 23:54:12 +0900

    스웨거 API 설정 클래스 수정

--- src/main/java/com/asdf/todo/config/ApiDocumentationConfig.java
+++ src/main/java/com/asdf/todo/config/ApiDocumentationConfig.java
@@ -16,5 +16,6 @@ public class ApiDocumentationConfig {
                                 .title("TODO List API")
                                 .version("1.0")
                                 .description("Spring Boot3를 이용한 TODO List API 문서"));
+        
     }
 }
